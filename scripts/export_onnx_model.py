5d417d7bf6490a6ed4193d47ede83ee0f3dd3ee9
zhaodehua <zhaodehua@taoche.com>
2023-07-07 15:04:53 +0800

    修改文件

--- scripts/export_onnx_model.py
+++ scripts/export_onnx_model.py
@@ -6,8 +6,8 @@
 
 import torch
 
-from segment_anything import sam_model_registry
-from segment_anything.utils.onnx import SamOnnxModel
+from sam_hq import sam_model_registry
+from sam_hq.utils.onnx import SamOnnxModel
 
 import argparse
 import warnings
